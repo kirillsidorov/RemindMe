5c7cf326389ec9e599e375d1ca9a1da39c632484
lubimo <sidorov.kirill198423@gmail.com>
2017-01-11 23:02:17 +0200

    Add NavigationView

--- app/src/main/java/com/sidorov/remindme/MainActivity.java
+++ app/src/main/java/com/sidorov/remindme/MainActivity.java
@@ -1,14 +1,15 @@
 package com.sidorov.remindme;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
