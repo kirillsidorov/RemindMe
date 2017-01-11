4b2600a77649f95c4256d89d0aff44b0625fdc5a
lubimo <sidorov.kirill198423@gmail.com>
2017-01-11 23:56:50 +0200

    finished NavigationView

--- app/src/main/java/com/sidorov/remindme/MainActivity.java
+++ app/src/main/java/com/sidorov/remindme/MainActivity.java
@@ -1,19 +1,24 @@
 package com.sidorov.remindme;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         
         initToolbar();
+        initNavigationVieW();
     }
 
     private void initToolbar() {
@@ -27,5 +32,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationVieW() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
     }
 }
